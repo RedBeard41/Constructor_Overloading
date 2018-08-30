33a29645bac8eaa2d7acbeea358b39354cb9ef5e
Gabriel Walters <gwalters@lhschools.org>
2018-08-29 22:36:01 -0400

    Created some constructor overloading

--- src/ConstructorOverloading.java
+++ src/ConstructorOverloading.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class ConstructorOverloading {
 
     public static void main(String[] args){
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter the desired time (in hours and minutes) separated by a space");
+        TimeOfDay now = new TimeOfDay(keyboard.nextInt(),keyboard.nextInt());
 
+
+        System.out.println("The current time of day is" +
+                " "+now.getmHour()+": "+now.getmMinutes());
 
 
     }
--- src/TimeOfDay.java
+++ src/TimeOfDay.java
@@ -1,2 +1,26 @@
 public class TimeOfDay {
+
+    private int mHour;
+    private int mMinutes;
+
+    public TimeOfDay() {
+        this(0,0);
+    }
+
+    public TimeOfDay(int pHour) {
+        this(pHour,0);
+    }
+
+    public TimeOfDay(int pHour, int pMinutes) {
+        this.mHour = pHour;
+        this.mMinutes = pMinutes;
+    }
+
+    public int getmHour() {
+        return mHour;
+    }
+
+    public int getmMinutes() {
+        return mMinutes;
+    }
 }
